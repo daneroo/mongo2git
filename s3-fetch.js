70a7dbf0b73b4a6bc34ea4a11aace605f06992dd
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-06-11 15:52:52 -0400

    tone down logging

--- s3-fetch.js
+++ s3-fetch.js
@@ -10,9 +10,6 @@ var S3 = awssum.load('amazon/s3').S3;
 var settingsFileName = 's3-settings.json';
 var settings = JSON.parse(require('fs').readFileSync(require('path').join(__dirname, settingsFileName), 'utf8'));
 
-// console.log('credentials',settings.credentials);
-console.log('endpoint', settings.endpoint);
-
 var s3 = new S3({
   accessKeyId: settings.credentials.mongoreader.accessKeyId,
   secretAccessKey: settings.credentials.mongoreader.secretAccessKey,
@@ -22,7 +19,7 @@ var s3 = new S3({
 // console.log('Region :', s3.region());
 // console.log('EndPoint :', s3.host());
 // console.log('AccessKeyId :', s3.accessKeyId());
-// console.log( 'SecretAccessKey :', s3.secretAccessKey() );
+// // console.log( 'SecretAccessKey :', s3.secretAccessKey() );
 // console.log('AwsAccountId :', s3.awsAccountId());
 if (0) s3.ListBuckets(function (err, data) {
   console.log("List buckets", err, data);
@@ -30,41 +27,40 @@ if (0) s3.ListBuckets(function (err, data) {
 
 var fetchOptions = {
   MaxKeys: 200,
-  Prefix: 'mongo-ekomobi_prod-201206',
+  // Prefix: 'mongo-ekomobi_prod-201206',
   BucketName: settings.endpoint.bucket
 };
 
 getAll(fetchOptions, function (err, results) {
-  function isDaily(f){
-    // match parts : *,db,date,hour,amazonId
-    var re=/^mongo-(.*)-([0-9]{8})-([0-9]{4})-(.*).tgz/;
-    var found = f.Key.match(re);
-    console.log('match',found,found[2]);
-    return found && found[3]==='0000';
-  }
-  results = results.filter(isDaily);
-  results.reverse();
-  
   if (err) {
     console.log('Error', JSON.stringify(err, null, 2));
     return;
   }
-  console.log('done reading ', results.length, 'objects');
+
+  function isDaily(f) { // filter for Dailys
+    // match parts : *,db,date,hour,amazonId
+    var re = /^mongo-(.*)-([0-9]{8})-([0-9]{4})-(.*).tgz/;
+    var found = f.Key.match(re);
+    return found && found[3] === '0000';
+  }
+  results = results.filter(isDaily);
+
+  // reverse order - newest first
+  results.reverse();
+
+  console.log('Will check and fetch ', results.length, 'objects');
 
   function iterator(f, next) {
-    showFileObject(f);
-    // next();
-    if (1) {
-      checkOne(f, function (err, ok) {
-        if (err) {
-          console.log('Error', JSON.stringify(err, null, 2));
-          next(err);
-        } else {
-          console.log('ok', ok);
-          next();
-        }
-      });
-    }
+    // showFileObject(f);
+    checkOne(f, function (err, ok) {
+      if (err) {
+        console.log('Error', JSON.stringify(err, null, 2));
+        next(err);
+      } else {
+        // console.log('ok', ok);
+        next();
+      }
+    });
   }
   async.forEachSeries(results, iterator, function (err) {
     console.log('done checking');
@@ -83,9 +79,9 @@ function checkOne(f, cb) {
   // console.log('checkOne',f);
   oETag = JSON.parse(f.ETag); // remove the extra ""
   var fileName = path.join(settings.snapshotDir, f.Key);
-  console.log('checking file', fileName, '==', oETag);
+  console.log('checking file', f.Key, '==', oETag);
 
-  if (path.existsSync(fileName)){
+  if (path.existsSync(fileName)) {
     var md5sum = crypto.createHash('md5');
     var s = fs.ReadStream(fileName);
     s.on('data', function (d) {
@@ -94,29 +90,24 @@ function checkOne(f, cb) {
 
     s.on('end', function () {
       var d = md5sum.digest('hex');
-      console.log(d + '  ' + fileName);
+      //console.log(d + '  ' + fileName);
       if (d === oETag) {
-        console.log('  ++ETag==md5: skipping');
+        //console.log('  ETag==md5: skipping');
         cb(null, 'OK');
-      } else {
-        console.log('  ++must download');
-        cb({
-          message: 'must download ' + fileName
-        });
+      } else { // download again
+        console.log('ETag != md5', fileName);
+        fetchOne(f, cb);
       }
     });
     s.on('error', function (exception) {
-      console.log('exception',exception);
+      console.log('exception', exception);
       cb({
         message: 'error reading ' + fileName
       });
     });
   } else {
-    console.log('file does not exist',fileName);
-    fetchOne(f,cb);
-    if(0)cb({
-      message: 'file not found ' + fileName
-    });
+    console.log('file does not exist', fileName);
+    fetchOne(f, cb);
   }
 }
 
@@ -136,10 +127,10 @@ function fetchOne(f, cb) {
       return;
     }
     console.log('Data', data);
-    var md5=crypto.createHash('md5').update(data.Body).digest("hex");
-    console.log('fetched file:%s length:%d md5:%s',fileName,data.Body.length,md5);
-    fs.writeFileSync(fileName,data.Body);
-    if (cb) cb(null,'OK');
+    var md5 = crypto.createHash('md5').update(data.Body).digest("hex");
+    console.log('fetched file:%s length:%d md5:%s', fileName, data.Body.length, md5);
+    fs.writeFileSync(fileName, data.Body);
+    if (cb) cb(null, 'OK');
   });
 }
 
@@ -152,7 +143,7 @@ function getAll(options, cb) { // cb(err,array)
   var results = [];
 
   function part(cb) {
-    console.log('part', options)
+    // console.log('part', options)
     s3.ListObjects(options, function (err, data) {
       if (err) cb(err);
       if (!data || !data.Body || !data.Body.ListBucketResult) {
@@ -165,7 +156,7 @@ function getAll(options, cb) { // cb(err,array)
         if (lbr.Contents) { // undefined for no objects
           results = results.concat(lbr.Contents);
         }
-        console.log('results now has', results.length, 'elements');
+        // console.log('results now has', results.length, 'elements');
         if (lbr.IsTruncated === 'true') {
           options.Marker = lbr.Contents[lbr.Contents.length - 1].Key;
           part(cb);
